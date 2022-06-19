91355641ea0bba059e2305e1c1d0ecb81804b40e
Nilson Roa <87431371+nilsondream@users.noreply.github.com>
2022-06-18 22:17:27 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Switch, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
+import useVH from 'react-viewport-height';
 import Menu from './components/Menu'
 import Home from './page/Home'
 import BlackWhite from './page/BlackWhite'
@@ -10,6 +11,8 @@ import CustomCursor from './components/CustomCursor'
 import './style/GlobalStyled.css'
 
 const App = () => {
+
+    useVH();
 
     const location = useLocation();
 
